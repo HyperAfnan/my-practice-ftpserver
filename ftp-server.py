a5897f1316334b26bb90e09e64da3a59e4a1bd17
Afnan <afnanmalik2712@gmail.com>
2024-09-16 21:29:24 +0530

    chore: Removed useless comments

--- ftp-server.py
+++ ftp-server.py
@@ -6,7 +6,6 @@ import os
 authorizer = DummyAuthorizer()
 
 # for login with password
-# authorizer.add_user("Afnan", "ammumalik", "/storage/emulated/0", perm="elradfmwMT")
 authorizer.add_user(os.getenv("USER"),os.getenv("PASSWORD"), "/storage/emulated/0", perm="elradfmwMT")
 
 # for without password login 
